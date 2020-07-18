b3ea07c17b872ab400e79fdfe8a75f7bac15bbb9
hninhsuhlaing <hsulaychocolate402@gmail.com>
2020-07-18 21:13:59 +0630

    Composite Custom View And Modified Custom View

---
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/RoundedImageView.kt
@@ -0,0 +1,13 @@
+package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
+
+class RoundedImageView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rounded_image_view)
+    }
+}
