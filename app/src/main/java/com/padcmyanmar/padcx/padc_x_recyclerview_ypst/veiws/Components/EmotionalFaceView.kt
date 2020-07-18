c86eaaf197dcdc2b6cc97ae7b315ff3bd6c9b3a8
hninhsuhlaing <hsulaychocolate402@gmail.com>
2020-07-18 22:41:42 +0630

    responsive Emotional Face View

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/Components/EmotionalFaceView.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/Components/EmotionalFaceView.kt
@@ -4,21 +4,45 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
 
 class EmotionalFaceView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private var faceColor = Color.YELLOW
-    private var eyeColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var borderColor = Color.BLACK
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyeColor = DEFAULT_EYE_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
 
 
-    private var borderWidth  = 4.0f
+    private var borderWidth  = DEFAULT_BORDER_WIDTH
 
-    private var size = 320
+    private var size = 0
 
     private  val mouthPatch = Path()
+    
+    var happinessState = HAPPY
+        set(state) {
+            field = state
+            invalidate()
+        }
+    init {
+        paint.isAntiAlias = true
+        setUpAttribute(attrs)
+    }
+
+    private fun setUpAttribute(attrs: AttributeSet?) {
+
+        context.withStyledAttributes(attrs, R.styleable.EmotionalFaceView){
+            happinessState = getString(R.styleable.EmotionalFaceView_state)?.toLongOrNull()?: HAPPY
+            faceColor = getColor(R.styleable.EmotionalFaceView_faceColor, DEFAULT_FACE_COLOR)
+            eyeColor = getColor(R.styleable.EmotionalFaceView_eyesColor, DEFAULT_EYE_COLOR)
+            mouthColor = getColor(R.styleable.EmotionalFaceView_mouthColor, DEFAULT_MOUTH_COLOR)
+            borderColor = getColor(R.styleable.EmotionalFaceView_borderColor, DEFAULT_BORDER_COLOR)
+            borderWidth = getDimension(R.styleable.EmotionalFaceView_borderWidth, DEFAULT_BORDER_WIDTH)
+        }
+    }
 
 
     override fun onDraw(canvas: Canvas) {
@@ -27,6 +51,13 @@ class EmotionalFaceView(context: Context?, attrs: AttributeSet?) : View(context,
         drawEyes(canvas)
         drawMouth(canvas)
     }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = measuredWidth.coerceAtMost(measuredHeight)
+        setMeasuredDimension(size,size)
+    }
+
 
     private fun drawMouth(canvas: Canvas) {
         mouthPatch.moveTo(size*0.22f, size*0.7f )
@@ -66,5 +97,16 @@ class EmotionalFaceView(context: Context?, attrs: AttributeSet?) : View(context,
         canvas.drawCircle(size/2f, size/2f, radius- borderWidth/2f, paint)
 
 
+    }
+
+    companion object{
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYE_COLOR = Color.BLACK
+        private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_WIDTH = 4.0F
+
+        const val  HAPPY = 0L
+        const val  SAD = 1L
     }
 }
